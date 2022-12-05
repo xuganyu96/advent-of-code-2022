62b9bb0a91265840d6b0a546eb791c320a933dcc
Ganyu "Bruce" Xu <xuganyu@berkeley.edu>
2022-12-04 23:54:09 -0800

    Cleaned up API

--- src/day5.rs
+++ src/day5.rs
@@ -1,23 +1,26 @@
 use std::fs;
 use std::collections::VecDeque;
+use std::cell::RefCell;
 
-/// a single stack
+/// A wrapper around a deque whose first element is the crate on the top of a
+/// stack, and whose last element is the crate on the bottom of a stack
 struct Stack {
     crates: VecDeque<char>,
 }
 
+/// Implement the methods needed to move crates around stacks
 impl Stack {
-    fn from_slice(s: &[char]) -> Self {
+    fn from_str(s: &str) -> Self {
         let mut crates = VecDeque::new();
-        for c in s {
-            crates.push_back(*c);
+        for c in s.chars() {
+            crates.push_back(c);
         }
 
         return Self{ crates };
     }
 
     /// Move the specified number of items from self to the other
-    fn move_crates(&mut self, to: &mut Stack, n: i32) {
+    fn move_crates(&mut self, to: &mut Stack, n: i32, preserve_ordering: bool) {
         let mut buffer: VecDeque<char> = VecDeque::new();
 
         for _ in 0..n {
@@ -26,52 +29,35 @@ impl Stack {
         }
 
         for _ in 0..n {
-            to.crates.push_front(buffer.pop_back().unwrap());
-        }
-    }
-
-    fn pop_crates(&mut self, n: i32) -> VecDeque<char> {
-        let mut buffer = VecDeque::new();
-        
-        for _ in 0..n {
-            buffer.push_back(self.crates.pop_front().unwrap());
-        }
-
-        return buffer;
-    }
-
-    fn push_crates(&mut self, mut buffer: VecDeque<char>, preserve_ordering: bool) {
-        let n = buffer.len();
-        for _ in 0..n {
             if preserve_ordering {
-                self.crates.push_front(buffer.pop_back().unwrap());
+                to.crates.push_front(buffer.pop_back().unwrap());
             } else {
-                self.crates.push_front(buffer.pop_front().unwrap());
+                to.crates.push_front(buffer.pop_front().unwrap());
             }
         }
     }
 }
 
-fn generate_stacks() -> Vec<Stack> {
-    let s1 = Stack::from_slice(&vec!['P', 'G', 'R', 'N']);
-    let s2 = Stack::from_slice(&vec!['C', 'D', 'G', 'F', 'L', 'B', 'T', 'J']);
-    let s3 = Stack::from_slice(&vec!['V', 'S', 'M']);
-    let s4 = Stack::from_slice(&vec!['P', 'Z', 'C', 'R', 'S', 'L']);
-    let s5 = Stack::from_slice(&vec!['Q', 'D', 'W', 'C', 'V', 'L', 'S', 'P']);
-    let s6 = Stack::from_slice(&vec!['S', 'M', 'D', 'W', 'N', 'T', 'C']);
-    let s7 = Stack::from_slice(&vec!['P', 'W', 'G', 'D', 'H']);
-    let s8 = Stack::from_slice(&vec!['V', 'M', 'C', 'S', 'H', 'P', 'L', 'Z']);
-    let s9 = Stack::from_slice(&vec!['Z', 'G', 'W', 'L', 'F', 'P', 'R']);
-
-    let stacks = vec![s1, s2, s3, s4, s5, s6, s7, s8, s9];
+fn generate_stacks() -> Vec<RefCell<Stack>> {
+    let stacks = vec![
+        RefCell::new(Stack::from_str("PGRN")),
+        RefCell::new(Stack::from_str("CDGHLBTJ")),
+        RefCell::new(Stack::from_str("VSM")),
+        RefCell::new(Stack::from_str("PZCRSL")),
+        RefCell::new(Stack::from_str("QDWCVLSP")),
+        RefCell::new(Stack::from_str("SMDWNTC")),
+        RefCell::new(Stack::from_str("PWGDH")),
+        RefCell::new(Stack::from_str("VMCSHPLZ")),
+        RefCell::new(Stack::from_str("ZGWLFPR")),
+    ];
     return stacks;
 }
 
-fn generate_test_stacks() -> Vec<Stack> {
+fn generate_test_stacks() -> Vec<RefCell<Stack>> {
     let stacks = vec![
-        Stack::from_slice(&vec!['N', 'Z']),
-        Stack::from_slice(&vec!['D', 'C', 'M']),
-        Stack::from_slice(&vec!['P']),
+        RefCell::new(Stack::from_str("NZ")),
+        RefCell::new(Stack::from_str("DCM")),
+        RefCell::new(Stack::from_str("P")),
     ];
 
     return stacks;
@@ -89,34 +75,37 @@ fn parse_command(line: &str) -> (usize, usize, i32) {
     return (from, to, n);
 }
 
-/// If I have a list of Stacks and I want to mutate two distinct elements
-/// within the same list. How do I do that?
-pub fn solve(input: &str) {
-    let mut stacks = generate_stacks();
-    let input = fs::read_to_string(input).unwrap();
+pub fn solve(input_path: &str) {
+    let stacks = generate_stacks();
+    let input = fs::read_to_string(input_path).unwrap();
     for line in input.lines() {
         if line.contains("move") {
-            let (fromi, toi, n) = parse_command(line);
-            let buffer = stacks.get_mut(fromi - 1).unwrap().pop_crates(n);
-            stacks.get_mut(toi - 1).unwrap().push_crates(buffer, false);
+            let (from, to, n) = parse_command(line);
+            let from = stacks.get(from - 1).unwrap();
+            let to = stacks.get(to - 1).unwrap();
+
+            from.borrow_mut().move_crates(&mut*to.borrow_mut(), n, false);
         }
     }
     for stack in &stacks {
-        print!("{}", stack.crates[0]);
+        print!("{}", stack.borrow().crates[0]);
     }
     println!();
 
-    let mut stacks = generate_stacks();
+    let stacks = generate_stacks();
+    let input = fs::read_to_string(input_path).unwrap();
     for line in input.lines() {
         if line.contains("move") {
-            let (fromi, toi, n) = parse_command(line);
-            let buffer = stacks.get_mut(fromi - 1).unwrap().pop_crates(n);
-            stacks.get_mut(toi - 1).unwrap().push_crates(buffer, true);
+            let (from, to, n) = parse_command(line);
+            let from = stacks.get(from - 1).unwrap();
+            let to = stacks.get(to - 1).unwrap();
+
+            from.borrow_mut().move_crates(&mut*to.borrow_mut(), n, true);
         }
     }
     for stack in &stacks {
-        print!("{}", stack.crates[0]);
+        print!("{}", stack.borrow().crates[0]);
     }
     println!();
+
 }
-
