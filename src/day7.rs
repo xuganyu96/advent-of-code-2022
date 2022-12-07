b450a1e70d6a9ac0a4e4a2c30f2b8bc547048898
Ganyu "Bruce" Xu <xuganyu@berkeley.edu>
2022-12-06 23:41:10 -0800

    Prettier code

--- src/day7.rs
+++ src/day7.rs
@@ -1,13 +1,13 @@
-use std::fs;
 use std::cell::RefCell;
+use std::fs;
 use std::rc::Rc;
 
 /// I need a part that parses the command and a part that models a file system
 
 #[derive(Debug)]
 enum Item {
-    Dir(String, String, Vec<Rc<RefCell<Item>>>),  // path, name, items
-    File(String, u64),  // filename, size
+    Dir(String, String, Vec<Rc<RefCell<Item>>>), // path, name, items
+    File(String, u64),                           // filename, size
 }
 
 impl Item {
@@ -15,7 +15,13 @@ impl Item {
         match self {
             Self::File(name, size) => format!("<File {} {}>", name, size),
             Self::Dir(path, name, items) => {
-                format!("<Dir {} {} nitems={} size={}>", path, name, items.len(), self.size())
+                format!(
+                    "<Dir {} {} nitems={} size={}>",
+                    path,
+                    name,
+                    items.len(),
+                    self.size()
+                )
             }
         }
     }
@@ -23,12 +29,13 @@ impl Item {
     /// Push a file onto dir.items
     fn push_file(&mut self, filename: &str, size: u64) {
         if let Self::Dir(_, _, items) = self {
-            items.push(
-                Rc::new(RefCell::new(Item::File(filename.to_string(), size)))
-            );
+            items.push(Rc::new(RefCell::new(Item::File(
+                filename.to_string(),
+                size,
+            ))));
         }
     }
-    
+
     fn push_dir(&mut self, dirname: &str) {
         if let Self::Dir(path, _, items) = self {
             let mut path = path.clone();
@@ -36,9 +43,11 @@ impl Item {
                 path.push_str("/");
             }
             path.push_str(dirname);
-            items.push(
-                Rc::new(RefCell::new(Item::Dir(path.to_string(), dirname.to_string(), vec![])))
-            );
+            items.push(Rc::new(RefCell::new(Item::Dir(
+                path.to_string(),
+                dirname.to_string(),
+                vec![],
+            ))));
         }
     }
 
@@ -60,13 +69,10 @@ impl Item {
         match self {
             Self::File(_, size) => return *size,
             Self::Dir(_, _, items) => {
-                return items.iter()
-                    .map(|item| item.borrow().size())
-                    .sum::<u64>();
+                return items.iter().map(|item| item.borrow().size()).sum();
             }
         }
     }
-
 }
 
 /// Recusively walk through all items and collect them into a single vector
@@ -93,7 +99,7 @@ fn flatten(cur: Rc<RefCell<Item>>) -> Vec<Rc<RefCell<Item>>> {
 fn parse_cd(cd: &str) -> String {
     let line = cd.lines().next().unwrap(); // cd spans one line
     let mut tokens = line.split(" ");
-    tokens.next().unwrap();  // cd
+    tokens.next().unwrap(); // cd
     return tokens.next().unwrap().to_string();
 }
 
@@ -104,7 +110,7 @@ fn parse_ls(ls: &str) -> (Vec<String>, Vec<(String, u64)>) {
     ls.lines().for_each(|line| {
         if line.contains("dir") {
             let mut tokens = line.split(" ");
-            tokens.next();  // "dir"
+            tokens.next(); // "dir"
             dirs.push(tokens.next().unwrap().to_string());
         } else if &line[..2] == "ls" {
             // do nothing
@@ -123,28 +129,36 @@ pub fn solve(input: &str) {
     // Parsing command and mutating the state
     let input = fs::read_to_string(input).unwrap();
 
-    let root = Rc::new(RefCell::new(Item::Dir("/".to_string(), "".to_string(), vec![])));
+    let root = Rc::new(RefCell::new(Item::Dir(
+        "/".to_string(),
+        "".to_string(),
+        vec![],
+    )));
     let mut cur = Rc::clone(&root);
-    let mut stack: Vec<Rc<RefCell<Item>>> = vec![];  // push and pop
-    
+    let mut stack: Vec<Rc<RefCell<Item>>> = vec![]; // push and pop
+
     input.split("$ ").for_each(|cmd| {
         if cmd.len() < 2 {
             ()
         } else if &cmd[0..2] == "ls" {
             let (dirs, files) = parse_ls(cmd);
-            dirs.iter().for_each(|dirname| cur.borrow_mut().push_dir(dirname));
-            files.iter().for_each(|(filename, size)| cur.borrow_mut().push_file(filename, *size));
-
+            dirs.iter()
+                .for_each(|dirname| cur.borrow_mut().push_dir(dirname));
+            files
+                .iter()
+                .for_each(|(filename, size)| cur.borrow_mut().push_file(filename, *size));
         } else if &cmd[..2] == "cd" {
             let dest = parse_cd(cmd);
             match &dest[..] {
                 "/" => {
                     cur = Rc::clone(&root);
-                    while stack.len() > 0 { stack.pop(); };
-                },
+                    while stack.len() > 0 {
+                        stack.pop();
+                    }
+                }
                 ".." => {
                     cur = stack.pop().unwrap();
-                },
+                }
                 _ => {
                     stack.push(Rc::clone(&cur));
                     let next = Rc::clone(&cur.borrow_mut().chdir(&dest));
@@ -173,10 +187,10 @@ pub fn solve(input: &str) {
                 if item_.size() >= threshold && item_.size() < min {
                     min = item_.size();
                 }
-            },
+            }
             Item::File(..) => {
                 // println!("{name} {size}");
-            },
+            }
         }
     }
     println!("{sum}");
