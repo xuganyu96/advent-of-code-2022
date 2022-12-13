43acb800abd319a7c8fb3250c624d41c162e2c75
Ganyu "Bruce" Xu <xuganyu@berkeley.edu>
2022-12-12 23:19:33 -0800

    Could not solve day13

---
+++ src/bin/day13.rs
@@ -0,0 +1,21 @@
+//! Day 13's problem is the first that I could not solve within 2 hours
+//!
+//! The main difficulty (using Rust) lies with parsing the inputs. I can come
+//! up with two major ways to parse them:
+//!
+//! - Implement my own data structure
+//! - Use some JSON reader
+//!
+//! The first approach carries with it the insurmountable challenge of writing
+//! an input parser (basically). The second approach was also not great since
+//! I am not familiar with the JSON parser packages. In any case, I ran out of
+//! time to go with either approach on the night this puzzle came out,
+//! but there are two interesting things to learn about that I hope to get to
+//! after the competition is fully over.
+//!
+//! 1. Write an actual JSON serializer and deserializer in Rust as a side proj
+//! 2. Learn to use the serde_json crate
+
+fn main() {
+    println!("Come back later!");
+}
